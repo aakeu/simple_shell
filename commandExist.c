970a04c06dd2c4f7632c305da358d618329a7f1a
aakeu <ibrahimakeukereke@outlook.com>
2023-10-16 10:45:17 -0700

    all done to betty standard

--- commandExist.c
+++ commandExist.c
@@ -1,35 +1,58 @@
 #include "shell.h"
 
-int the_command_exists(const char *command) {
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *dir = strtok(path_copy, ":");
+/**
+ * the_command_exists - Check if a command exists in the system's
+ * PATH directories.
+ * @command: The command to check for existence.
+ *
+ * Description:
+ * This function takes a command as input and checks if it exists
+ * in any of the directories listed in the system's PATH
+ * environment variable. It iterates through the PATH directories,
+ * constructs the full path to the command, and checks if the
+ * command is executable. If the command is found and is
+ * executable, the function returns 1; otherwise, it returns 0.
+ *
+ * Return: 1 if the command exists and is executable; 0 otherwise.
+ */
 
-    while (dir != NULL) {
-        char full_path[512];
-        
-        /* Manually combining strings */
-        int i, j;
-        for (i = 0; dir[i] != '\0'; i++) {
-            full_path[i] = dir[i];
-        }
-        full_path[i] = '/';
-        i++;
-        for (j = 0; command[j] != '\0'; j++, i++) {
-            full_path[i] = command[j];
-        }
-        full_path[i] = '\0';
+int the_command_exists(const char *command)
+{
+	char *path = getenv("PATH");
+	char *path_copy = strdup(path);
+	char *dir = strtok(path_copy, ":");
 
-	ModesterBola_print_two("Checking path: %s\n", full_path);
+	while (dir != NULL)
+	{
+		char full_path[512];
 
-        if (access(full_path, F_OK | X_OK) == 0) {
-            free(path_copy);
-            return 1;
-        }
+		/* Manually combining strings */
+		int i, j;
 
-        dir = strtok(NULL, ":");
-    }
+		for (i = 0; dir[i] != '\0'; i++)
+		{
+			full_path[i] = dir[i];
+		}
+		full_path[i] = '/';
+		i++;
+		for (j = 0; command[j] != '\0'; j++, i++)
+		{
+			full_path[i] = command[j];
+		}
+		full_path[i] = '\0';
 
-    free(path_copy);
-    return 0;
+		ModesterBola_print_two("Checking path: %s\n", full_path);
+
+		if (access(full_path, F_OK | X_OK) == 0)
+		{
+
+			free(path_copy);
+			return (1);
+		}
+
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (0);
 }
--- shell.c
+++ shell.c
@@ -1,51 +1,95 @@
 #include "shell.h"
 #define TESTING_GETLINE 0
 
+
+/**
+ * main - Entry point for the custom shell.
+ *
+ * Description:
+ * This function serves as the entry point for a custom shell program.
+ * It continuously displays a shell prompt, reads user input, and
+ * executes the entered command.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+
 int main(void)
 {
-        char command[152];
-        char **tokens;
+	char command[152];
+	char **tokens;
 
-        while (true)
-        {
-                display_the_prompt();
-                tokens = read_the_given_command(command, sizeof(command));
-                execute_the_command(tokens);
-        }
-        return 0;
+	while (true)
+	{
+		display_the_prompt();
+		tokens = read_the_given_command(command, sizeof(command));
+		execute_the_command(tokens);
+
+	}
+	return (0);
 }
 
-char *construct_path(const char *dir, const char *command) {
-    int i, j;
-    char *result = malloc(512);
+/**
+ * construct_path - Concatenate directory and command to form a full path.
+ * @dir: The directory path.
+ * @command: The command name.
+ *
+ * Description:
+ * This function takes a directory path and a command name and concatenates
+ * them to form a full path. It allocates memory for the result and returns
+ * it.
+ *
+ * Return: A pointer to the concatenated path, or NULL on failure.
+ */
 
-    if (!result) {
-        perror("malloc");
-        exit(EXIT_FAILURE);
-    }
+char *construct_path(const char *dir, const char *command)
+{
+	int i, j;
+	char *result = malloc(512);
 
-    for (i = 0; dir[i] != '\0'; i++) {
-        result[i] = dir[i];
-    }
-    result[i] = '/';
-    i++;
-    for (j = 0; command[j] != '\0'; j++, i++) {
-        result[i] = command[j];
-    }
-    result[i] = '\0';
+	if (!result)
+	{
+		perror("malloc");
+		exit(EXIT_FAILURE);
+	}
 
-    return result;
+	for (i = 0; dir[i] != '\0'; i++)
+	{
+		result[i] = dir[i];
+	}
+	result[i] = '/';
+	i++;
+	for (j = 0; command[j] != '\0'; j++, i++)
+	{
+		result[i] = command[j];
+	}
+	result[i] = '\0';
+
+	return (result);
 }
 
 #if TESTING_GETLINE
+
+/**
+ * main - Entry point for testing the custom getline function.
+ *
+ * Description:
+ * This function serves as the entry point for testing the custom
+ * getline function, `my_getline`. It reads lines from standard input
+ * using `my_getline`, prints them, and frees the allocated memory
+ * for each line.
+ *
+ * Return: Always returns 0 to indicate successful execution.
+ */
+
 int main(void)
 {
-    char *line;
-    while ((line = my_getline(STDIN_FILENO)))
-    {
-        printf("Read: %s", line);
-        free(line);
-    }
-    return 0;
+	char *line;
+
+	while ((line = my_getline(STDIN_FILENO)))
+	{
+		ModesterBola_print("Read: %s", line);
+		free(line);
+	}
+	return (0);
 }
 #endif
